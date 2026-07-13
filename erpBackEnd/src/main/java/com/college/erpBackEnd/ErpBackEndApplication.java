a48869b7c9237d69e720bd5b8b1233ca925f450e
Piyush Pardhi <pardhipiyush07>
2026-07-13 15:05:22 +0530

    day1 commit 1

---
+++ erpBackEnd/src/main/java/com/college/erpBackEnd/ErpBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.college.erpBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpBackEndApplication.class, args);
+	}
+
+}
---
+++ erpBackEnd/src/test/java/com/college/erpBackEnd/ErpBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.erpBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
